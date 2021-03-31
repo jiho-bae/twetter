9bf6da25ffed4b83459fabb86d29faca8bd5973e
jiho-bae <67041709+jiho-bae@users.noreply.github.com>
2021-03-31 20:29:30 +0900

    set an absolute import and use a firebase-authentication.

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,10 @@
-import AppRouter from "./Routers";
+import { useState } from "react";
+import AppRouter from "components/Routers";
+import { authService } from "fbase";
 
 function App() {
-  return <AppRouter />;
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  return <AppRouter isLoggedIn={isLoggedIn} />;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
-import firebase from "./firebase";
-console.log(firebase);
+import App from "components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
