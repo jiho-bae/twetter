e7c71bc5b1ccdf043e0616339b817ee845626db8
jiho-bae <67041709+jiho-bae@users.noreply.github.com>
2021-03-31 22:38:57 +0900

    make LogIn and CreateAccount functions. We add init state to wait for react wihle firebase is loading.

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AppRouter from "components/Routers";
 import { authService } from "fbase";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
-  return <AppRouter isLoggedIn={isLoggedIn} />;
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
+  return (
+    <>
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing..."}
+      <footer>&copy; {new Date().getFullYear()} Twetter</footer>
+    </>
+  );
 }
 
 export default App;
