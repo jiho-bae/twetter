ef250cabcff35931b0d282fffcab3702775a5d72
jiho-bae <67041709+jiho-bae@users.noreply.github.com>
2021-04-06 16:47:32 +0900

    get all twetts from my firestore and show them at home.

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,38 +1,61 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { dbService } from "fbase";
 
 const Home = () => {
-  const [tweet, setTweet] = useState("");
+  const [twett, setTwett] = useState("");
+  const [twetts, setTwetts] = useState("");
+  const getTwetts = async () => {
+    const dbTwetts = await dbService.collection("twetts").get();
+    dbTwetts.forEach((document) => {
+      const twettObject = {
+        ...document.data(),
+        id: document.id,
+      };
+      setTwetts((prev) => [twettObject, ...prev]);
+    });
+  };
+  useEffect(() => {
+    getTwetts();
+  }, []);
 
   const onSubmit = async (event) => {
     event.preventDefault();
-    if (tweet !== "") {
-      await dbService.collection("tweets").add({
-        tweet,
+    if (twett !== "") {
+      await dbService.collection("twetts").add({
+        twett,
         createdAt: Date.now(),
       });
-      setTweet("");
+      setTwett("");
     }
   };
   const onChange = (event) => {
     const {
       target: { value },
     } = event;
-    setTweet(value);
+    setTwett(value);
   };
   return (
     <div>
       <form onSubmit={onSubmit}>
         <input
-          value={tweet}
+          value={twett}
           onChange={onChange}
           type="text"
           placeholder="What's on your mind?"
           maxLength={120}
         />
-        <input type="submit" value="Tweet" />
+        <input type="submit" value="Twett" />
       </form>
+      <div>
+        {twetts &&
+          twetts.map((twett) => (
+            <div key={twett.id}>
+              <h4>{twett.twett}</h4>
+            </div>
+          ))}
+      </div>
     </div>
   );
 };
+
 export default Home;
