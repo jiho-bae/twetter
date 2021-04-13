5f4b5f17b5a9379cf6bad1083f20007c77eff3dd
jiho-bae <67041709+jiho-bae@users.noreply.github.com>
2021-04-13 14:51:42 +0900

    create a preview when uploading a picture

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,6 +5,7 @@ import Twett from "components/Twett";
 const Home = ({ userObj }) => {
   const [twett, setTwett] = useState("");
   const [twetts, setTwetts] = useState("");
+  const [fileUrl, setFileUrl] = useState();
 
   useEffect(() => {
     dbService.collection("twetts").onSnapshot((snapshot) => {
@@ -33,6 +34,23 @@ const Home = ({ userObj }) => {
     } = event;
     setTwett(value);
   };
+
+  const onFileChange = (event) => {
+    const {
+      target: { files },
+    } = event;
+    const theFile = files[0];
+    const reader = new FileReader();
+    reader.onloadend = (finishedEvent) => {
+      const {
+        currentTarget: { result },
+      } = finishedEvent;
+      setFileUrl(result);
+    };
+    reader.readAsDataURL(theFile);
+  };
+
+  const onClearFileUrlClick = () => setFileUrl(null);
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -43,8 +61,14 @@ const Home = ({ userObj }) => {
           placeholder="What's on your mind?"
           maxLength={120}
         />
-        <input type="file" accept="image/*" />
+        <input type="file" accept="image/*" onChange={onFileChange} />
         <input type="submit" value="Twett" />
+        {fileUrl && (
+          <div>
+            <img src={fileUrl} width="50px" height="50px" />
+            <button onClick={onClearFileUrlClick}>Clear</button>
+          </div>
+        )}
       </form>
       <div>
         {twetts &&
