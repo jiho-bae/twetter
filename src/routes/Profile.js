67872fdbfe20bee3395ca64ec8eb08b2d4ec7ed3
jiho-bae <67041709+jiho-bae@users.noreply.github.com>
2021-04-14 22:20:05 +0900

    update displayName on the profile page, but it needs to be refreshed.

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,27 +1,42 @@
-import { authService, dbService } from "fbase";
-import React, { useEffect } from "react";
+import { authService } from "fbase";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 
 const Profile = ({ userObj }) => {
+  const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
+
   const history = useHistory();
   const onLogOutClick = () => {
     authService.signOut();
     history.push("/");
   };
-  const getMyTwetts = async () => {
-    const twetts = await dbService
-      .collection("twetts")
-      .where("creatorId", "==", userObj.uid)
-      .orderBy("createdAt")
-      .get();
-    console.log(twetts.docs.map((doc) => doc.data()));
+
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewDisplayName(value);
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    if (userObj.displayName !== newDisplayName) {
+      await userObj.updateProfile({
+        displayName: newDisplayName,
+      });
+    }
   };
 
-  useEffect(() => {
-    getMyTwetts();
-  }, []);
   return (
     <>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          type="text"
+          placehodler="Display Name"
+          value={newDisplayName}
+        />
+        <input type="submit" value="Update Profile" />
+      </form>
       <button onClick={onLogOutClick}>Log Out</button>
     </>
   );
